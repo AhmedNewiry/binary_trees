3d511f37a8de5983a400f59e562cfb6cba909b4d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-12-15 14:47:58 -0800

    Find the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binarty_tree_uncle - finds the uncle of a node
+ * binary_tree_uncle - finds the uncle of a node
  * @node: is a pointer to the node to find the uncle
  * Return: a pointer to the uncle node or NULL
  */
